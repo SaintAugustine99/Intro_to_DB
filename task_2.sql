6d28f4c5547f706461196cae5d9d10df0b0119ba
Onserio Ogeto <onserioogeto@gmail.com>
2025-01-30 13:45:30 +0300

    the creation of a book store database

--- task_2.sql
+++ task_2.sql
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS Authors (
 );
 
 -- Create the books table
-CREATE TABLE IF NOT EXISTS books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     author_id INT,
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS books (
 );
 
 -- Create the customers table
-CREATE TABLE IF NOT EXISTS customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS customers (
 );
 
 -- Create the orders table
-CREATE TABLE IF NOT EXISTS orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS orders (
 );
 
 -- Create the order_details table
-CREATE TABLE IF NOT EXISTS order_details (
+CREATE TABLE IF NOT EXISTS Order_details (
     order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
