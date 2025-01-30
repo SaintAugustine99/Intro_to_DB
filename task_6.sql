8fcf0233a8b25d699bd5aadd724904dfd1fc60cd
Onserio Ogeto <onserioogeto@gmail.com>
2025-01-30 14:27:25 +0300

    adding more rows into customer tables

--- task_6.sql
+++ task_6.sql
@@ -2,6 +2,6 @@
 
 INSERT INTO customers ( customer_name, customer_id, first_name, last_name, email, address)
 VALUES,
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+(customer_id 2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+(customer_id 3,'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+(customer_id 4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
