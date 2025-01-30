5ccbad29dcc4a8064a139caf91b4fd271354c5e6
Onserio Ogeto <onserioogeto@gmail.com>
2025-01-30 13:44:46 +0300

    the creation of a book store database

--- task_2.sql
+++ task_2.sql
@@ -7,8 +7,8 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
 -- Create the authors table
-CREATE TABLE IF NOT EXISTS authors (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Authors (
+    author_name INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     birthdate DATE,
     nationality VARCHAR(100)
