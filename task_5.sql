41acf102b797069dc948e637155618ee3916503c
Onserio Ogeto <onserioogeto@gmail.com>
2025-01-30 14:18:29 +0300

    adding a single row in the database

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,4 @@
 -- Insert a single row into the customers table
 
-INSERT INTO customers (customer_name, first_name, last_name, email, address)
+INSERT INTO customers (customer_name,  customer_id, first_name, last_name, email, address)
 VALUES (1, 'Cole', 'Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
